fe32c57e91f1dc1a7313c70f1d52854a97df4b9d
Cezarine <96452545+Cezarine@users.noreply.github.com>
2023-08-03 18:36:55 -0300

    Init Project

---
+++ FilmesAPI/FilmesAPI/Controllers/MovieController.cs
@@ -0,0 +1,37 @@
+﻿using FilmesAPI.Models;
+using Microsoft.AspNetCore.Mvc;
+using System.Collections.Generic;
+
+namespace FilmesAPI.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class MovieController
+    {
+        private static List<MovieVM> Movies = new List<MovieVM>();
+        private static int vID = 0;
+
+        [HttpPost]
+        public void CreateMovie([FromBody] MovieVM pMovie)
+        {
+            pMovie.ID = vID++;
+            Movies.Add(pMovie);
+            Console.WriteLine(pMovie.Title);
+            Console.WriteLine(pMovie.Duration);
+        }
+
+        [HttpGet]
+        public IEnumerable<MovieVM> GetAllMovies()
+        {
+            return Movies;
+        }
+
+        [HttpGet("{id}")]
+        public MovieVM? GetMovieFromID(int id )
+        {
+            return Movies.FirstOrDefault(Movie => Movie.ID == id);
+        }
+
+    }
+}
+
---
+++ FilmesAPI/FilmesAPI/Models/MovieVM.cs
@@ -0,0 +1,22 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Diagnostics.CodeAnalysis;
+
+namespace FilmesAPI.Models
+{
+    public class MovieVM
+    {
+        public int ID { get; set; }
+
+        [Required (ErrorMessage = "The title is requerid")]
+        [MaxLength (100)]
+        public String Title { get; set; }
+
+        [Required(ErrorMessage = "The gender is requerid")]
+        [MaxLength (30, ErrorMessage = "Length cannot exceed 30 characters")]
+        public String Gender { get; set; } //Genero
+
+        [Required(ErrorMessage = "The duration is requerid")]
+        [Range(70, 600, ErrorMessage = "The duration must be between 70 and 600 minutes")]
+        public int Duration { get; set; }
+    }
+}
